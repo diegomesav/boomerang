7bbf23b93516c3c454f6fdaa94768cdcff3d4424
diegomesav <diegomesav0130@gmail.com>
2023-07-09 07:21:45 -0500

    add getBasketTotal

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,20 @@
 import React from "react";
+import {List,Typography} from '@material-ui/core'
+import { useStateValue } from "../StateProvider";
+import { getBasketTotal } from "../reducer";
 
 
 const Review = (nextStep, backStep) => {
+  const[{basket}, dispatch] = useStateValue();
   return(
     <div>
-        <h1>review</h1>
+      <Typography variant='h6' gutterBottom>
+        Order Summary
+      </Typography>
+      <List disablePadding>
+        {getBasketTotal(basket)}
+      </List>
+        
     </div>
   )
 }
